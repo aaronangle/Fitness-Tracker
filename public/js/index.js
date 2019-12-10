58676f95fb07a151950963f1d6c6929b22151699
Aaron Angle <acangle10@gmail.com>
2019-12-10 00:36:31 -0700

    begin charting data

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,9 @@ const reps = document.querySelector("#reps");
 const workout = document.querySelector("#workout")
 const add = document.querySelector("#add")
 const viewWorkout = document.querySelectorAll(".viewWorkout")
+const visualize = document.querySelectorAll(".visualize")
+const repNumber = document.querySelector("#repNumber")
+const setNumber = document.querySelector("#setNumber")
 
 if (addWorkout) {
     addWorkout.addEventListener("click", function (event) {
@@ -108,4 +111,38 @@ if (submit) {
             window.location.assign("/")
         })
     });
+}
+
+if (visualize) {
+    visualize.forEach(element => {
+        element.addEventListener("click", function (event) {
+            event.preventDefault();
+            if (repNumber) {
+                const title = this.parentNode.parentNode.childNodes[1].innerHTML;
+                const rep = this.parentNode.childNodes[1].childNodes[1].innerHTML;
+                const set = this.parentNode.childNodes[3].childNodes[1].innerHTML;
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var chart = new Chart(ctx, {
+                    // The type of chart we want to create
+                    type: 'line',
+
+                    // The data for our dataset
+                    data: {
+                        labels: ["Reps"],
+
+                        datasets: [{
+                            label: title,
+                            // backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [rep, set]
+                        }]
+                    },
+
+                    // Configuration options go here
+                    options: {},
+
+                });
+            }
+        })
+    })
 }
